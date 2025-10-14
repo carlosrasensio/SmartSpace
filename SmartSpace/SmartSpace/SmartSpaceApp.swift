5cc54837a7ccb49a15584396862a6ac7459dc860
Carlos Rodriguez Asensio <carlosrasensio@gmail.com>
2025-10-14 16:44:08 +0200

    Create Xcode project

---
+++ SmartSpace/SmartSpace/SmartSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartSpaceApp.swift
+//  SmartSpace
+//
+//  Created by Carlos Rodriguez Asensio on 14/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
